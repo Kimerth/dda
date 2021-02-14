bf5da6c8d67ff8b339e17560ea890a0a496330ce
Kimerth <motfoleas@yahoo.com>
2021-02-14 11:57:53 +0200

    perceptron fit wrapper

--- models/perceptron.py
+++ models/perceptron.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Embedding, Dense, ReLU, Activation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import KLDivergence
 
 
 class Perceptron(tf.Module):
@@ -17,7 +19,7 @@ class Perceptron(tf.Module):
 
         self.label_range = label_range
 
-    def __call__(self, s1, s2):
+    def data_embedding(self, s1, s2):
         e1 = self.embedding(s1)
         e1 = tf.math.reduce_mean(e1, 0)
         e2 = self.embedding(s2)
@@ -26,10 +28,24 @@ class Perceptron(tf.Module):
         sub = tf.math.abs(e2 - e1)
         prod = e1 * e2
 
-        return self.model(tf.stack((sub, prod), 1))
+        return tf.stack((sub, prod), 1)
 
-    def encode(self, label):
+    def encode_label(self, label):
         min_l, max_l = self.label_range
 
         y = label - min_l
         y /= max_l - min_l
+
+    def __call__(self, s1, s2):
+        return self.model(self.data_embedding(s1, s2))
+
+class Neural(Perceptron):
+    def __init__(self, vocab, output_sizes, emb_dim=300, label_range=(0,5), optimizer='adam', loss='kl_divergence'):
+        super().__init__(vocab, emb_dim, output_sizes, label_range)
+
+        self.model.compile(optimizer=optimizer, loss=loss)
+
+    def fit(self, train_data, labels, epochs=5):
+        X, Y = list(map(self.data_embedding, train_data)), list(map(self.encode_label, labels))
+
+        self.model.fit(X, Y, epochs=epochs, use_multiprocessing=True)
